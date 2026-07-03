97e8d1799ed766258472ec1d4883248f5eb2f68c
olamilekan5162 <opeyemiolalekan481@gmail.com>
2026-07-03 14:12:57 +0100

    refactor sorting algorithms: update selection sort logic and add bubble and merge sort implementations

--- sort.js
+++ sort.js
@@ -1,31 +1,6 @@
-// Merge sort
+const array = [38, 27, 43, 3, 9, 82, 10, 62];
 
-const array = [38, 27, 43, 3, 9, 82, 10];
-
-const maxValue = (array) => {
-  let maxIndex = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > array[maxIndex]) {
-      maxIndex = i;
-    }
-  }
-  return maxIndex;
-};
-
-const sortArray = (array) => {
-  let sortedArray = [];
-  const length = array.length;
-  for (let i = 0; i < length; i++) {
-    let index = maxValue(array);
-    sortedArray.push(array[index]);
-    array.splice(index, 1);
-  }
-
-  return sortedArray;
-};
-
-console.log(sortArray(array));
-
+// selection sort
 const selectionSort = (array) => {
   let result = [];
   let tempArray = [...array];
@@ -33,7 +8,7 @@ const selectionSort = (array) => {
   while (tempArray.length > 0) {
     let maxIndex = 0;
     for (let i = 0; i < tempArray.length; i++) {
-      if (tempArray[i] > tempArray[maxIndex]) {
+      if (tempArray[i] < tempArray[maxIndex]) {
         maxIndex = i;
       }
     }
@@ -45,3 +20,60 @@ const selectionSort = (array) => {
 };
 
 console.log(selectionSort(array));
+
+//bubble sort
+const bubbleSort = (array) => {
+  let tempArray = [...array];
+
+  for (let i = 0; i < tempArray.length; i++) {
+    for (let j = 0; j < tempArray.length - 1 - i; j++) {
+      if (tempArray[j] > tempArray[j + 1]) {
+        [tempArray[j], tempArray[j + 1]] = [tempArray[j + 1], tempArray[j]];
+      }
+    }
+  }
+
+  return tempArray;
+};
+
+console.log(bubbleSort(array));
+
+// merge sort
+
+const merge = (leftArray, rightArray) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] < rightArray[j]) {
+      result.push(leftArray[i]);
+      i++;
+    } else {
+      result.push(rightArray[j]);
+      j++;
+    }
+  }
+
+  while (i < leftArray.length) {
+    result.push(leftArray[i]);
+    i++;
+  }
+
+  while (j < rightArray.length) {
+    result.push(rightArray[j]);
+    j++;
+  }
+
+  return result;
+};
+
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+  const mid = Math.floor(array.length / 2);
+  let [left, right] = [array.slice(0, mid), array.slice(mid)];
+  let sortedLeft = mergeSort(left);
+  let sortedRight = mergeSort(right);
+  return merge(sortedLeft, sortedRight);
+};
+
+console.log(mergeSort(array));
