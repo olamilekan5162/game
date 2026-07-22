a025b99c47ceacbf9787246b7df07731002c4be4
olamilekan5162 <opeyemiolalekan481@gmail.com>
2026-07-22 12:59:06 +0100

    add some test logic

--- index.js
+++ index.js
@@ -66,8 +66,69 @@
 //   level: josh.getLevel(),
 // });
 
-const array = [38, 27, 43, 3, 9, 82, 10, 62];
+// const array = [38, 27, 43, 3, 9, 82, 10, 62];
 
-let newArray = array.sort((a, b) => a - b);
+// let newArray = array.sort((a, b) => a - b);
 
-console.log(newArray);
+// console.log(newArray);
+
+// const userName = ["ola", "josh", "mike", "john", "james"];
+// const password = ["123", "456", "789", "000", "111"];
+
+// const user = "mikee";
+// const pass = "789";
+
+// let userFound = false;
+
+// for (let i = 0; i < userName.length; i++) {
+//   if (userName[i] === user) {
+//     console.log("User found");
+//     userFound = true;
+//     if (password[i] === pass) {
+//       console.log("User authenticated");
+//     } else {
+//       console.log("Incorrect password");
+//     }
+//     break;
+//   }
+// }
+
+// if (!userFound) {
+//   console.log("User not found");
+// }
+
+const users = [
+  {
+    username: "loko",
+    password: "789",
+  },
+  {
+    username: "yinka",
+    password: "456",
+  },
+  {
+    username: "ola",
+    password: "789",
+  },
+];
+const user = "mike";
+const pass = "798";
+
+let userFound = false;
+
+for (let i = 0; i < users.length; i++) {
+  if (users[i].username === user) {
+    console.log("User found");
+    userFound = true;
+    if (users[i].password === pass) {
+      console.log("User authenticated");
+    } else {
+      console.log("Incorrect password");
+    }
+    break;
+  }
+}
+
+if (!userFound) {
+  console.log("User not found");
+}
